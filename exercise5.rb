949240fa25f45982a329fb641ce66d72c47ed0c6
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-17 14:04:28 -0400

    Define Method to Convert Temp

--- exercise5.rb
+++ exercise5.rb
@@ -6,3 +6,9 @@ puts ""
 
 puts "Enter a Temperature in Fahrenheit:"
 fahr = gets.chomp.to_i
+
+def convert_farenheit_to_celsius(fahr)
+    c = (fahr-32) * 5/9
+    puts "#{fahr}F equals #{c}C"
+    return c
+end
