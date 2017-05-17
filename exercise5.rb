8036b959b02e68b11aa20c6b90e0fb565b73d3cd
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-17 14:01:12 -0400

    Complete Exercise,Create exercise5 gets input

---
+++ exercise5.rb
@@ -0,0 +1,8 @@
+# Exercise 6
+
+puts "Temperature Control"
+puts ""
+puts ""
+
+puts "Enter a Temperature in Fahrenheit:"
+fahr = gets.chomp.to_i
---
+++ ruby_FUN2_exercises.rb
@@ -0,0 +1,18 @@
+puts "Ruby FUN II"
+puts ""
+puts""
+
+# EXERCISE 5
+
+puts "***EXERCISE FIVE***"
+
+def greet_backwards(name)
+
+  puts "Hello #{name.reverse}#{name.reverse}! Welcome Homo!"
+
+end
+
+greet_backwards("Bob")
+greet_backwards("Shirly")
+greet_backwards("Sue")
+greet_backwards("Andy")
