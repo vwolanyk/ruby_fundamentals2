6a19d1a89fd1d8617a3ab6d9e242ef65f9b3d7fd
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-17 15:35:40 -0400

    Create doc, student hash, display method - works

---
+++ exercise10.rb
@@ -0,0 +1,18 @@
+# EXERCISE 10
+
+puts "****EXERCISE TEN****"
+puts ""
+puts ""
+
+# Create Hash of Students
+
+students = {cohort1: 34, cohort2: 42, cohort3: 22}
+puts students
+
+# 2
+
+def name_number_students(cohort_hash)
+    cohort_hash.each {|name,num| puts "#{name}: #{num} students" }
+end
+
+name_number_students(students)
