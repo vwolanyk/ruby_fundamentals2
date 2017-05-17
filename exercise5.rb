10d627e412c77e6ae4b2da78accd0fb99c9f02cc
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-05-17 14:09:12 -0400

    Call Method with User input, tested for err

--- exercise5.rb
+++ exercise5.rb
@@ -7,8 +7,10 @@ puts ""
 puts "Enter a Temperature in Fahrenheit:"
 fahr = gets.chomp.to_i
 
-def convert_farenheit_to_celsius(fahr)
-    c = (fahr-32) * 5/9
-    puts "#{fahr}F equals #{c}C"
+def convert_farenheit_to_celsius(temp)
+    c = (temp-32) * 5/9
+    puts "#{temp}F equals #{c}C"
     return c
 end
+
+convert_farenheit_to_celsius(fahr)
